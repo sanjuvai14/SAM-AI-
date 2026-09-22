687e6838d76188ce3192c638fdc7c7226fe268ec
sanjuvai14 <sanjoybanik6251@gmail.com>
2026-09-22 19:50:19 +0600

    fix: improve Android voice recognition feedback

--- android/app/src/main/java/com/sam/privateai/MainActivity.kt
+++ android/app/src/main/java/com/sam/privateai/MainActivity.kt
@@ -36,6 +36,7 @@ class MainActivity : Activity() {
         val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "bn-BD")
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to SAM")
         startActivityForResult(intent, requestCode)
     }
 
